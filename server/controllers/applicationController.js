f76e2baaa68f045bbe12c37bd181ed852d27bb35
kapishan <kapishan.official@gmail.com>
2026-08-23 17:22:43 +0530

    add applicationController

---
+++ server/controllers/applicationController.js
@@ -0,0 +1,37 @@
+const Application = require("../models/Application");
+
+const submitApplication = async (req,res) => {
+    try{
+        const{ applicantType,fullName,companyName,nicOrRegNo,email,phone,address,membershipType,} = req.body;
+         if (!applicantType || !nicOrRegNo || !email || !phone || !address || !membershipType){
+                    return res.status(400).json({message: "please provide applicantType,nicOrRegNo,email,phone,address and membershipType"});
+                }
+              
+        if (applicantType === "INDIVIDUAL" && !fullName) {
+                    return res.status(400).json({message: "full name is required for indidual applicant"})
+                }
+        
+        if (applicantType === "COMPANY" && !companyName) {
+                    return res.status(400).json({message: "company name is required for company applicant"})
+                }      
+        
+        const application = await Application.create({
+            userId: req.user._id,
+            applicantType,
+            fullName,
+            companyName,
+            nicOrRegNo,
+            email,
+            phone,
+            address,
+            membershipType,
+        });
+        res.status(201).json(application);
+    }
+    catch (error){
+        res.status(500).json({message: "server error", error: error.message});
+
+    }
+};
+
+module.exports = {submitApplication};
