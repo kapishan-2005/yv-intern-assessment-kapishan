e2954e158ecb6cdf91e2587dd17da3176fc1c9ec
kapishan <kapishan.official@gmail.com>
2026-08-23 17:00:10 +0530

    add application model

---
+++ server/models/Application.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const applicationSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        applicantType: {
+            type: String,
+            enum: ["INDIVIDUAL", "COMPANY"],
+            required: true,
+        },
+        fullName:{
+            type: String,
+        },
+        companyName: {
+            type: String,
+        },
+        nicOrRegNo: {
+            type: String,
+        },
+         email:{
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        }, 
+        membershipType:{
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            enum:["PENDING","APPROVED","REJECTED"],
+        },
+        rejectionReason: {
+            type: String,
+            default: null,
+        },
+         reviewedBy:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            default: null,
+        },
+    },
+    {timestamps: true }
+);
+
+module.exports = mongoose.model("Application", applicationSchema);
