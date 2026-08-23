e3549d943225ec772bc9b5b752564b8dfc0391c5
kapishan <kapishan.official@gmail.com>
2026-08-23 20:22:10 +0530

    add userController

---
+++ server/controllers/userController.js
@@ -0,0 +1,44 @@
+const User = require("../models/User");
+const officerRole = require("../models/officerRole");
+
+const assignOfficerRole = async (req,res) =>
+{
+    try{
+        const {userId, officerRoleId} = req.body;
+
+        if (!userId || officerRoleId) {
+            return res.status(400).json({message: "user and officerRoleId are required"});
+        }
+
+        const role = await officerRole.findById(officerRoleId);
+        if (!role) {
+            return res.status(404).json({message: "officer role not found"});
+        }
+
+        const user = await User.findByIdAndUpdate(
+            userId,
+            {role: "OFFICER", officerRoleId: officerRoleId},
+            {new: true}
+        ).select("-password");
+
+        if (!user) {
+            return res.status(404).json({message: "User not found"});
+        }
+        res.status(200).json(user);
+    }
+    catch(error) {
+        res.status(500).json({message: "server error", error: error.message});
+    }
+};
+
+const getAllUsers = async (req,res) => {
+    try{
+        const users =await User.find().select("-password");
+        res.status(200).json(users);
+    }
+    catch(error) {
+        res.status(500).json({message: "server error", error: error.message});
+    }
+};
+
+module.exports = {assignOfficerRole, getAllUsers};
