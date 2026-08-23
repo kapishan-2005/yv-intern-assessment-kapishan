799675c0a841f79f71bb043bccec9959646072aa
kapishan <kapishan.official@gmail.com>
2026-08-23 17:50:38 +0530

    add membership application submit and fixed server.js route / error

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
-
+const applicationRoutes = require("./routes/applicationRoutes")
 connectDB();
 const app=express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-
+app.use("/api/applications", applicationRoutes);
 app.get("/",(req,res) => {
     res.send("API running");
 });
