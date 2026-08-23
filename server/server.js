36ea75c55c0ba0d8e33b70d47a749fe768a56111
kapishan <kapishan.official@gmail.com>
2026-08-23 10:42:32 +0530

    connect mongodb and run the server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./conflig/db");
+
+connectDB();
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res) => {
+    res.send("API running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`);
+});
