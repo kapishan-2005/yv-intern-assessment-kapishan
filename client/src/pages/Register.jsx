427132268856e40ba2cf4048a77dcc7ed9cbbb10
kapishan <kapishan.official@gmail.com>
2026-08-24 11:04:46 +0530

    login,register pages connect in app.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import {useState} from "react";
 import {useNavigate, Link } from "react-router-dom";
 import api from "../services/api";
-import {useAuth} from "../context/Authcontext";
+import {useAuth} from "../context/AuthContext";
 
 const Register = () => {
     const [name, setName] = useState("");
