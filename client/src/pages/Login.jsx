40d9230664aedb4e461d59ea43013250c635b3fc
kapishan <kapishan.official@gmail.com>
2026-08-24 10:27:17 +0530

    add login and register pages

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,59 @@
+import {useState} from "react";
+import {useNavigate, Link } from "react-router-dom";
+import api from "../services/api";
+import {useAuth} from "../context/Authcontext";
+
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const {login} = useAuth();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+        setLoading(true);
+        try{
+            const res = await api.post("/auth/login", {email, password});
+            login(res.data);
+
+            if (res.data.role === "CHAIRMAN" || res.data.role === "OFFICER") {
+                navigate("/staff/dashboard");
+            }
+            else{
+                navigate("/member/dashboard");
+            }
+        }
+        catch (err) {
+            setError(err.response?.data?.message || "Login failed");
+        }
+        finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div style={{maxwidth: 400, margin: "50px auto"}}>
+            <h2>Login</h2>
+            {error && <p style={{color: "red"}}>{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email</label>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                </div>
+                <div>
+                    <label>password</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                </div>
+                <button type="submit" disable={loading}>
+                    {loading ? "Logging in..." : "Login"}
+                </button>
+            </form>
+            <p>Don't have an account? <Link to ="/register">Register</Link></p>
+        </div>
+    );
+};
+
+export default Login;
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,59 @@
+import {useState} from "react";
+import {useNavigate, Link } from "react-router-dom";
+import api from "../services/api";
+import {useAuth} from "../context/Authcontext";
+
+const Register = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const {login} = useAuth();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+        setLoading(true);
+        try{
+            const res = await api.post("/auth/login", {email, password});
+            login(res.data);
+             navigate("/member/dashboard")
+           
+        }
+        catch (err) {
+            setError(err.response?.data?.message || "Registration failed");
+        }
+        finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div style={{maxwidth: 400, margin: "50px auto"}}>
+            <h2>Register</h2>
+            {error && <p style={{color: "red"}}>{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Name</label>
+                    <input type="text" value={name} onChange={(e) => setEmail(e.target.value)} required />
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                </div>
+                <div>
+                    <label>password</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required minLength={8} />
+                </div>
+                <button type="submit" disable={loading}>
+                    {loading ? "Logging in..." : "Login"}
+                </button>
+            </form>
+            <p>Already have an account? <Link to ="/login">Login</Link></p>
+        </div>
+    );
+};
+
+export default Register;
